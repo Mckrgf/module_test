7ae975b6e259885120d8c38b9c1b29a637e36068
Mckrgf <1124507487@qq.com>
2021-06-28 20:52:23 +0800

    添加子模块两个。 next: 新建router，实现模块间的跳转

--- module_test_002/src/main/java/com/yaobing/framemvpproject/XJBWActivity.kt
+++ module_test_002/src/main/java/com/yaobing/framemvpproject/XJBWActivity.kt
@@ -1,9 +1,11 @@
-package www.supcon.com.module_test_002
+package com.yaobing.framemvpproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yaobing.module_apt.Router
 import com.yaobing.module_middleware.activity.BaseActivity
 
+@Router("asdf")
 class XJBWActivity : BaseActivity() {
     override fun getLayoutID(): Int {
         return R.layout.activity_x_j_b_w
